5912b329f483b531639a7657895438ae1d5517cc
kemaltf <kemaltaufikf@gmail.com>
2023-11-19 21:37:08 +0700

    react suspense for loading sceleton instead just using loading skeleton

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -27,6 +27,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
+  console.log('Fetching revenue data...');
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
